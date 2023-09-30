1d3b6c7b8808eb533c0cf087fc47b5125c9c160f
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-09-30 13:45:16 -0700

    refactored and getting closer

--- notation.js
+++ notation.js
@@ -28,7 +28,7 @@ const createMeasure = (X, Y, staveWidth, numBeats, beatValue, notesArray) => {
   stave.setContext(context).draw();
   const voice = new Voice({ num_beats: numBeats, beat_value: beatValue });
   let allNotes = notesArray.flatMap((note, idx) => note.notation());
-
+  console.log(allNotes);
   voice.addTickables(allNotes);
   const formatter = new Vex.Flow.Formatter()
     .joinVoices([voice])
@@ -48,34 +48,42 @@ const notes1 = [
   {
     noteName: "Low A#",
     notation: () => [createNewNote("a#/3", "q", "#")],
+    path: "alto_sax_sounds/Low-Bb.mp3",
   },
   {
     noteName: "Low B",
     notation: () => [createNewNote("b/3", "q")],
+    path: "alto_sax_sounds/Low-B.mp3",
   },
   {
     noteName: "Low C",
     notation: () => [createNewNote("c/4", "q")],
+    path: "alto_sax_sounds/Low-C.mp3",
   },
   {
     noteName: "low C#",
     notation: () => [createNewNote("c#/4", "q", "#")],
+    path: "alto_sax_sounds/Low-Db.mp3",
   },
   {
     noteName: "low D",
     notation: () => [createNewNote("d/4", "q")],
+    path: "alto_sax_sounds/Low-D.mp3",
   },
   {
     noteName: "low D#",
     notation: () => [createNewNote("d#/4", "q", "#")],
+    path: "alto_sax_sounds/Low-Eb.mp3",
   },
   {
     noteName: "low E",
     notation: () => [createNewNote("e/4", "q")],
+    path: "alto_sax_sounds/Low-E.mp3",
   },
   {
     noteName: "low F",
     notation: () => [createNewNote("f/4", "q")],
+    path: "alto_sax_sounds/Low-F.mp3",
   },
 ];
 
@@ -83,68 +91,84 @@ const notes2 = [
   {
     noteName: "low F#",
     notation: () => [createNewNote("f#/4", "q", "#")],
+    path: "alto_sax_sounds/Low-Gb.mp3",
   },
   {
     noteName: "middle G",
     notation: () => [createNewNote("g/4", "q")],
+    path: "alto_sax_sounds/Middle-G.mp3",
   },
   {
     noteName: "middle G#",
     notation: () => [createNewNote("g#/4", "q", "#")],
+    path: "alto_sax_sounds/Middle-Ab.mp3",
   },
   {
     noteName: "middle A",
     notation: () => [createNewNote("a/4", "q")],
+    path: "alto_sax_sounds/Middle-A.mp3",
   },
   {
     noteName: "middle Bb",
     notation: () => [createNewNote("a#/4", "q", "#")],
+    path: "alto_sax_sounds/Middle-Bb.mp3",
   },
   {
     noteName: "middle B",
     notation: () => [createNewNote("b/4", "q")],
+    path: "alto_sax_sounds/Middle-B.mp3",
   },
   {
     noteName: "middle C",
     notation: () => [createNewNote("c/5", "q")],
+    path: "alto_sax_sounds/Middle-C.mp3",
   },
   {
     noteName: "middle C#",
     notation: () => [createNewNote("c#/5", "q", "#")],
+    path: "alto_sax_sounds/Middle-Db.mp3",
   },
 ];
 const notes3 = [
   {
     noteName: "middle D",
     notation: () => [createNewNote("d/5", "q")],
+    path: "alto_sax_sounds/Middle-D.mp3",
   },
   {
     noteName: "middle D#",
     notation: () => [createNewNote("d#/5", "q", "#")],
+    path: "alto_sax_sounds/Middle-Eb.mp3",
   },
   {
     noteName: "middle E",
     notation: () => [createNewNote("e/5", "q")],
+    path: "alto_sax_sounds/Middle-E.mp3",
   },
   {
     noteName: "middle F",
     notation: () => [createNewNote("f/5", "q")],
+    path: "alto_sax_sounds/Middle-F.mp3",
   },
   {
     noteName: "middle F#",
     notation: () => [createNewNote("f#/5", "q", "#")],
+    path: "alto_sax_sounds/High-Gb.mp3",
   },
   {
     noteName: "high G",
     notation: () => [createNewNote("g/5", "q")],
+    path: "alto_sax_sounds/High-G.mp3",
   },
   {
     noteName: "high G#",
     notation: () => [createNewNote("g#/5", "q", "#")],
+    path: "alto_sax_sounds/High-Ab.mp3",
   },
   {
     noteName: "high A",
     notation: () => [createNewNote("a/5", "q")],
+    path: "alto_sax_sounds/High-A.mp3",
   },
 ];
 
@@ -152,82 +176,52 @@ const notes4 = [
   {
     noteName: "high A#",
     notation: () => [createNewNote("a#/5", "q", "#")],
+    path: "alto_sax_sounds/High-A#.mp3",
   },
   {
     noteName: "high B",
     notation: () => [createNewNote("b/5", "q")],
+    path: "alto_sax_sounds/High-B.mp3",
   },
   {
     noteName: "high C",
     notation: () => [createNewNote("c/6", "q")],
+    path: "alto_sax_sounds/High-C.mp3",
   },
   {
     noteName: "high C#",
     notation: () => [createNewNote("c#/6", "q", "#")],
+    path: "alto_sax_sounds/High-Db.mp3",
   },
   {
     noteName: "high D",
     notation: () => [createNewNote("d/6", "q")],
+    path: "alto_sax_sounds/High-D.mp3",
   },
   {
     noteName: "high D#",
     notation: () => [createNewNote("d#/6", "q", "#")],
+    path: "alto_sax_sounds/High-Eb.mp3",
   },
   {
     noteName: "high E",
     notation: () => [createNewNote("e/6", "q")],
+    path: "alto_sax_sounds/High-E.mp3",
   },
   {
     noteName: "high F",
     notation: () => [createNewNote("f/6", "q")],
+    path: "alto_sax_sounds/High-F.mp3",
   },
 ];
+
+const allNotesArray = [...notes1, ...notes2, ...notes3, ...notes4];
 
 //X, Y, staveWidth, numBeats, beatValue, notesArray
 createMeasure(10, 0, 600, 8, 4, notes1);
 createMeasure(10, 0, 600, 8, 4, notes2);
 createMeasure(10, 0, 600, 8, 4, notes3);
 createMeasure(10, 0, 600, 8, 4, notes4);
-
-// const svgElements = document.getElementsByTagName("svg")
-// const gtags = document.getElementsByTagName("g")
-// console.log(svgElements[0])
-// console.log(gtags)
-
-const allNotes = [
-  { noteName: "Low Bb", path: "alto_sax_sounds/Low-Bb.mp3" },
-  { noteName: "Low B", path: "alto_sax_sounds/Low-B.mp3" },
-  { noteName: "Low C", path: "alto_sax_sounds/Low-C.mp3" },
-  { noteName: "Low C#", path: "alto_sax_sounds/Low-Db.mp3" },
-  { noteName: "Low D", path: "alto_sax_sounds/Low-D.mp3" },
-  { noteName: "Low D#", path: "alto_sax_sounds/Low-Eb.mp3" },
-  { noteName: "Low E", path: "alto_sax_sounds/Low-E.mp3" },
-  { noteName: "Low F", path: "alto_sax_sounds/Low-F.mp3" },
-  { noteName: "Low F#", path: "alto_sax_sounds/Low-Gb.mp3" },
-  { noteName: "Middle G", path: "alto_sax_sounds/Middle-G.mp3" },
-  { noteName: "Middle G#", path: "alto_sax_sounds/Middle-Ab.mp3" },
-  { noteName: "Middle A", path: "alto_sax_sounds/Middle-A.mp3" },
-  { noteName: "Middle A#", path: "alto_sax_sounds/Middle-Bb.mp3" },
-  { noteName: "Middle B", path: "alto_sax_sounds/Middle-B.mp3" },
-  { noteName: "Middle C", path: "alto_sax_sounds/Middle-C.mp3" },
-  { noteName: "Middle C#", path: "alto_sax_sounds/Middle-Db.mp3" },
-  { noteName: "Middle D", path: "alto_sax_sounds/Middle-D.mp3" },
-  { noteName: "Middle D#", path: "alto_sax_sounds/Middle-Eb.mp3" },
-  { noteName: "Middle E", path: "alto_sax_sounds/Middle-E.mp3" },
-  { noteName: "Middle F", path: "alto_sax_sounds/Middle-F.mp3" },
-  { noteName: "High F#", path: "alto_sax_sounds/High-Gb.mp3" },
-  { noteName: "High G", path: "alto_sax_sounds/High-G.mp3" },
-  { noteName: "High G#", path: "alto_sax_sounds/High-Ab.mp3" },
-  { noteName: "High A", path: "alto_sax_sounds/High-A.mp3" },
-  { noteName: "High A#", path: "alto_sax_sounds/High-A#.mp3" },
-  { noteName: "High B", path: "alto_sax_sounds/High-B.mp3" },
-  { noteName: "High C", path: "alto_sax_sounds/High-C.mp3" },
-  { noteName: "High C#", path: "alto_sax_sounds/High-Db.mp3" },
-  { noteName: "High D", path: "alto_sax_sounds/High-D.mp3" },
-  { noteName: "High D#", path: "alto_sax_sounds/High-Eb.mp3" },
-  { noteName: "High E", path: "alto_sax_sounds/High-E.mp3" },
-  { noteName: "High F", path: "alto_sax_sounds/High-F.mp3" },
-];
 
 const audioContext = new AudioContext();
 
@@ -238,7 +232,6 @@ const playSound = (url) => {
     .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer))
     .then((audioBuffer) => {
       const sourceNode = audioContext.createBufferSource();
-      console.log(sourceNode);
       sourceNode.buffer = audioBuffer;
       sourceNode.connect(audioContext.destination);
       sourceNode.start();
@@ -248,12 +241,16 @@ const playSound = (url) => {
     });
 };
 
+const allSVGgs = document.querySelectorAll("svg g");
+
 const attachNoteClickListener = () => {
-  for (let g of document.querySelectorAll("svg g")) {
-    g.addEventListener("click", () => {
-      playSound(allNotes[0].path);
-    });
-  }
+  allSVGgs.forEach((g, idx) => {
+    if (allNotesArray[idx]) {
+      g.addEventListener("click", () => {
+        playSound(allNotesArray[idx].path);
+      });
+    }
+  });
 };
 
 attachNoteClickListener();
